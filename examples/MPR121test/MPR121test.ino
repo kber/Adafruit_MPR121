63c16a28a1e5032eb6a0ef8f77294fcdea57c5ce
lukywong <lukywong@gmail.com>
2017-08-30 12:52:20 +0800

    Modify change small config

--- examples/MPR121test/MPR121test.ino
+++ examples/MPR121test/MPR121test.ino
@@ -27,12 +27,9 @@ uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
 
 void setup() {
-  Serial.begin(9600);
+  while (!Serial);        // needed to keep leonardo/micro from starting too fast!
 
-  while (!Serial) { // needed to keep leonardo/micro from starting too fast!
-    delay(10);
-  }
-  
+  Serial.begin(9600);
   Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
   
   // Default address is 0x5A, if tied to 3.3V its 0x5B
